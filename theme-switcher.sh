eeac2ac91c365e8b52dc854003e50b0f5681f016
catzy007 <catzy.rais@gmail.com>
2019-04-28 21:00:14 +0700

    initial commit

---
+++ theme-switcher.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+file1=~/.config/tea-switcher-mode.cfg
+file2=/usr/share/tealinux/ThemeSwitcher/tea-switcher.sh
+if [ ! -f ${file1} ]; then
+	echo 0 > ${file1}
+fi
+readarray -t apps < ${file1}
+if [ ${apps[0]} == 0 ]; then
+        kill $(pgrep yad)
+        yad --notification --image="switcher-light" --command="${file2}" --text="Switch to Dark Mode" &
+        xfconf-query -c xsettings -p /Net/ThemeName -s Tea-Light
+        xfconf-query -c xsettings -p /Net/IconThemeName -s Papirus-Light
+        xfconf-query -c xfwm4 -p /general/theme -s Tea-Light
+        xfconf-query -c xsettings -p /Gtk/CursorThemeName -s Tea-Cursor-Dark
+        echo 1 > ${file1}
+else
+        kill $(pgrep yad)
+        yad --notification --image="switcher-dark" --command="${file2}" --text="Switch to Light Mode" &
+        xfconf-query -c xsettings -p /Net/ThemeName -s Tea-Dark
+        xfconf-query -c xsettings -p /Net/IconThemeName -s Papirus-Dark
+        xfconf-query -c xfwm4 -p /general/theme -s Tea-Dark
+        xfconf-query -c xsettings -p /Gtk/CursorThemeName -s Tea-Cursor-Light
+        echo 0 > ${file1}
+fi
+#xfce4-panel -r
