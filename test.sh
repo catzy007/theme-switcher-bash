ce5baca04899a35cf6aca6993c352f04dc9cf1e1
Bagus Saputra <38147269+catzy007@users.noreply.github.com>
2019-04-28 16:30:58 +0000

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+file1=~/.config/tea-switcher-mode.cfg
+file2=/usr/share/tealinux/ThemeSwitcher/theme-switcher.sh
+if [ ! -f ${file1} ]; then
+	echo 1 > ${file1}
+fi
+readarray -t apps < ${file1}
+if [ ${apps[0]} == 0 ]; then
+        kill $(pgrep yad)
+        yad --notification --image="switcher-light" --command="${file2}" --text="Switch to Dark Mode" --no-middle &
+        xfconf-query -c xsettings -p /Net/ThemeName -s Tea-Light
+        xfconf-query -c xsettings -p /Net/IconThemeName -s Papirus
+        xfconf-query -c xfwm4 -p /general/theme -s Tea-Light
+        xfconf-query -c xsettings -p /Gtk/CursorThemeName -s Tea-Cursor-Dark
+	xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/last-image -s "/usr/share/tealinux/wallpaper/defaults.jpg"
+        echo 1 > ${file1}
+else
+        kill $(pgrep yad)
+        yad --notification --image="switcher-dark" --command="${file2}" --text="Switch to Light Mode" --no-middle &
+        xfconf-query -c xsettings -p /Net/ThemeName -s Tea-Dark
+        xfconf-query -c xsettings -p /Net/IconThemeName -s Papirus-Dark
+        xfconf-query -c xfwm4 -p /general/theme -s Tea-Dark
+        xfconf-query -c xsettings -p /Gtk/CursorThemeName -s Tea-Cursor-Light
+	xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/last-image -s "/usr/share/tealinux/wallpaper/Tealinux Robotic v3 - Bagus Saputra.png"
+        echo 0 > ${file1}
+fi
+#xfce4-panel -r
