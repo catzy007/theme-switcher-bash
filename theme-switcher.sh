d667ef7b65339f93f4c1916f65c0f271c883f5b6
catzy007 <catzy.rais@gmail.com>
2019-05-13 22:34:28 +0700

    minor fix

--- theme-switcher.sh
+++ theme-switcher.sh
@@ -8,16 +8,12 @@ fi
 
 readarray -t apps < ${file1}
 if [ ${apps[0]} == 0 ]; then
-        kill $(pgrep yad)
-        yad --notification --image="switcher-light" --command="${file2}" --text="Switch to Dark Mode" --no-middle &
         xfconf-query -c xsettings -p /Net/ThemeName -s Tea-Light
         xfconf-query -c xsettings -p /Net/IconThemeName -s Papirus
         xfconf-query -c xfwm4 -p /general/theme -s Tea-Light
         xfconf-query -c xsettings -p /Gtk/CursorThemeName -s Tea-Cursor-Dark
         echo 1 > ${file1}
 else
-        kill $(pgrep yad)
-        yad --notification --image="switcher-dark" --command="${file2}" --text="Switch to Light Mode" --no-middle &
         xfconf-query -c xsettings -p /Net/ThemeName -s Tea-Dark
         xfconf-query -c xsettings -p /Net/IconThemeName -s Papirus-Dark
         xfconf-query -c xfwm4 -p /general/theme -s Tea-Dark
